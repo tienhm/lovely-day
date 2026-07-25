87a08cfdffb844630014a73e412cedc248192cf4
Hoang Minh Tien <tienhm@gmail.com>
2026-07-25 08:36:22 +0200

    Fix build-ff: create .zip then rename to .xpi

--- build-ff.bat
+++ build-ff.bat
@@ -3,11 +3,13 @@ setlocal
 
 set NAME=a-lovely-day
 set DIST=dist
+set ZIP=%DIST%\%NAME%-firefox.zip
 set OUT=%DIST%\%NAME%-firefox.xpi
 set TMP=_build_tmp_ff
 
 if not exist %DIST% mkdir %DIST%
 if exist %OUT% del %OUT%
+if exist %ZIP% del %ZIP%
 if exist %TMP% rmdir /s /q %TMP%
 mkdir %TMP%
 
@@ -20,7 +22,8 @@ copy background.js     %TMP%\              > nul
 xcopy icons    %TMP%\icons\    /e /i /q > nul
 xcopy _locales %TMP%\_locales\ /e /i /q > nul
 
-powershell -NoProfile -Command "Compress-Archive -Path '%TMP%\*' -DestinationPath '%OUT%' -Force"
+powershell -NoProfile -Command "Compress-Archive -Path '%TMP%\*' -DestinationPath '%ZIP%' -Force"
+rename %ZIP% %NAME%-firefox.xpi
 
 rmdir /s /q %TMP%
 echo [OK] %OUT%
